b3706d5a1edc3ba6db7a39403256513deb2cfdbe
Vansh-Vionsys <Technowebdev045454>
2024-05-27 11:58:41 +0530

    servises and industries updated

---
+++ src/app/(pages)/industries/industriesD.js
@@ -0,0 +1,94 @@
+
+export const ourIndustries = [
+    {
+      title: "Banking",
+      imagesrc: "/assets/industries/CardsIphoto/banking.jpg",
+      description: "Empower your financial institution with our tailored solutions. From secure transactions to regulatory compliance, we support your banking operations for optimal performance and customer satisfaction.",
+      href1: "",
+    },
+    {
+      title: "E-commerce",
+      imagesrc: "/assets/industries/CardsIphoto/E-commerce.jpg",
+      description: "Drive online success with our customized solutions. From seamless shopping experiences to secure payment processing, we help you navigate the digital marketplace with ease and efficiency.",
+      href1: "",
+    },
+    {
+      title: "Healthcare",
+      imagesrc: "/assets/industries/CardsIphoto/Madical.jpg",
+      description: "Transform patient care with our innovative solutions. From electronic health records to telemedicine platforms, we empower healthcare providers to deliver quality services with efficiency and precision.",
+      href1: "",
+    },
+    {
+      title: "Education",
+      imagesrc: "/assets/industries/CardsIphoto/Education.jpg",
+      description: "Enhance learning experiences with our innovative solutions. From personalized platforms to administrative tools, we support educational institutions for success.",
+      href1: "",
+    },
+    {
+      title: "Pharma",
+      imagesrc: "/assets/industries/CardsIphoto/pharma.jpg",
+      description: "We offer specialized services in pharmaceutical development, ensuring compliance, innovation, and efficiency to accelerate the journey from discovery to market.",
+      href1: "",
+    },
+    {
+      title: "Insurance",
+      imagesrc: "/assets/industries/CardsIphoto/Insurance.jpg",
+      description: "We provide tailored insurance solutions, combining risk management expertise with innovative technologies to protect and grow your assets effectively.",
+      href1: "",
+    },
+    {
+      title: "Telecommunication",
+      imagesrc: "/assets/industries/CardsIphoto/telicon.jpg",
+      description: "Elevate connectivity with our advanced solutions. From network optimization to customer engagement tools, we empower telecom companies for seamless communication experiences.",
+      href1: "",
+    },
+    {
+      title: "Manufacturing",
+      imagesrc: "/assets/industries/CardsIphoto/manufacturer.jpg",
+      description: "Drive efficiency with our customized solutions. From process optimization to supply chain management, we empower manufacturers for streamlined operations.",
+      href1: "",
+    },
+    {
+      title: "Real Estate",
+      imagesrc: "/assets/industries/CardsIphoto/realEstate.jpg",
+      description: "Transform property management with our innovative solutions. From listings management to client portals, we empower real estate professionals for success in a competitive market.",
+      href1: "",
+    },
+    {
+      title: "Finance",
+      imagesrc: "/assets/industries/CardsIphoto/Finance.jpg",
+      description: "Transform property management with our innovative solutions. From listings management to client portals, we empower real estate professionals for success in a competitive market.",
+      href1: "",
+    },
+    {
+      title: "Logistics and Transportation",
+      imagesrc: "/assets/industries/CardsIphoto/transport.jpg",
+      description: "Optimize logistics with our tailored solutions. From route optimization to fleet management, we empower transportation companies for efficient and reliable operations.",
+      href1: "",
+    },
+    {
+      title: "Professional Services",
+      imagesrc: "/assets/industries/CardsIphoto/profesional.jpg",
+      description: "Optimize logistics with our tailored solutions. From route optimization to fleet management, we empower transportation companies for efficient and reliable operations.",
+      href1: "",
+    },
+    {
+      title: "IT Digital Transformation",
+      imagesrc: "/assets/industries/CardsIphoto/transport.jpg",
+      description: "Optimize logistics with our tailored solutions. From route optimization to fleet management, we empower transportation companies for efficient and reliable operations.",
+      href1: "",
+    },
+    {
+      title: "Construction",
+      imagesrc: "/assets/industries/CardsIphoto/COnstruct.jpg",
+      description: "Optimize logistics with our tailored solutions. From route optimization to fleet management, we empower transportation companies for efficient and reliable operations.",
+      href1: "",
+    },
+    {
+      title: "Field Service",
+      imagesrc: "/assets/industries/CardsIphoto/Field.jpg",
+      description: "Optimize logistics with our tailored solutions. From route optimization to fleet management, we empower transportation companies for efficient and reliable operations.",
+      href1: "",
+    },
+  ];
+  
