3f49f1a71b8cb92c2e0068104b36fca448865b7c
VijayaVionsys <Vijayagavhane@vionsys.com>
2024-05-29 10:09:43 +0530

    working on insurance page

--- src/app/ui/industries/Insurance/Hero.jsx
+++ src/app/ui/industries/Insurance/Hero.jsx
@@ -5,6 +5,8 @@ import React from "react";
 const Hero = () => {
   return (
     <section className=" w-[100vw]">
+
+      {/* section1 */}
       <div className="w-full ">
         <Image src={Insurance} alt="" className="w-full" />
       </div>
@@ -13,6 +15,24 @@ const Hero = () => {
           <p className=" text-5xl font-extrabold text-black">Insurance</p>
         </div>
       </div>
+
+      {/* section2 */}
+      <div className="w-full py-3">
+        <div className="flex flex-col gap-5 justify-center items-center">
+          <h1 className="text-4xl font-bold text-blue-500">Overview</h1>
+          <p className="w-[50%] text-center text-lg font-normal text-gray-800">
+            Understanding the unique challenges and opportunities within the
+            insurance industry is our specialty. Our suite of software solutions
+            is designed to streamline operations, enhance customer experiences,
+            and drive innovation. Whether you're an insurer, broker, or agent,
+            our technology empowers you to stay ahead in a rapidly evolving
+            market.
+          </p>
+        </div>
+      </div>
+
+      
+
     </section>
   );
 };
