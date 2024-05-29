d62d8ee387f0761741e06aba7b2587cbc7fdbff4
VijayaVionsys <Vijayagavhane@vionsys.com>
2024-05-29 12:12:09 +0530

    Working on Insurance page in Industries

--- src/app/(pages)/industries/Insurance/page.jsx
+++ src/app/(pages)/industries/Insurance/page.jsx
@@ -1,10 +1,14 @@
 import Hero from "@/app/ui/industries/Insurance/Hero";
+import WhatWeProvide from "@/app/ui/industries/Insurance/WhatWeProvide";
+import WhyChooseUs from "@/app/ui/industries/Insurance/WhyChooseUs";
 import React from "react";
 
 const page = () => {
   return (
-    <div>
+    <div className="overflow-x-hidden">
       <Hero />
+      <WhatWeProvide />
+      <WhyChooseUs />
     </div>
   );
 };
