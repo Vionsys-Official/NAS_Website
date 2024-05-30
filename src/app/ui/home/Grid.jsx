d026b64f03a7627bac46d46dd048dce991cc9f72
ranjitgund <ranjitgund@vionsys.com>
2024-05-30 19:00:28 +0530

    Framer motion updated, Home page updated

--- src/app/ui/home/Grid.jsx
+++ src/app/ui/home/Grid.jsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 import React from "react";
-import { fadein } from '@/components/ui/variants';
-import { motion } from 'framer-motion';
+import { fadein } from "@/components/ui/variants";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import customcentric from "./images/customcentric.jpg";
 import development from "./images/development.jpg";
@@ -16,8 +16,14 @@ const Grid = () => {
       <section className="h-full w-full">
         <div className="px-4 md:py-20 py-6 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-14">
           {/* discover our unique features */}
-          <div className="flex flex-col mb-6 lg:justify-evenly lg:flex-row  ">
-            <h2 className="max-w-lg mb-5 text-blue-900 font-sans text-SubHeading font-bold tracking-tight  sm:text-4xl sm:leading-none md:mb-6 group text-center">
+          <div className="flex flex-col mb-6 py-6 lg:justify-evenly lg:flex-row bg-stone-100 rounded-md shadow-xl ">
+            <motion.h2
+              variants={fadein("left", 0.1)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: true, amount: 0.5 }}
+              className="max-w-lg  text-blue-900 font-sans text-SubHeading font-bold tracking-tight  sm:text-3xl   group text-center"
+            >
               <span className="inline-block mb-1 sm:mb-4">
                 Discover Our Unique Features
                 <br className="hidden md:block" />
@@ -25,21 +31,27 @@ const Grid = () => {
                 streamline your workflow.
               </span>
               <div className="h-1 ml-auto duration-300 origin-left transform bg-deep-purple-accent-400 scale-x-30 group-hover:scale-x-100" />
-            </h2>
-            <p className="tracking-wide lg:text-Para lg:max-w-xl text-center md:border-l-4 md:pl-8 border-green-400 p-2 text-black ">
+            </motion.h2>
+            <motion.p
+              variants={fadein("right", 0.1)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: true, amount: 0.5 }}
+              className="tracking-wide lg:text-Para lg:max-w-xl text-center md:border-l-4 md:pl-8 border-green-400 p-2 text-black "
+            >
               Explore the innovative features that set us apart. Our advanced IT
               solutions are designed to optimize your operations and enhance
               productivity. With cutting-edge technology, seamless integration,
               and robust security measures, our features ensure your business
               stays ahead in a competitive market. Discover how our
               user-friendly interfaces, customizable options.
-            </p>
+            </motion.p>
           </div>
           {/* first row */}
           <div className="grid gap-6 row-gap-5  lg:grid-cols-3 sm:row-gap-6 sm:grid-cols-2 md:py-10">
             {/* first */}
             <Link href="/" aria-label="View Item">
-              <h1 className="text-center py-2 text-CardHeading border-t-2 border-blue-600 rounded-md text-blue-700 font-extrabold">
+              <h1 className="text-center py-2 text-CardHeading border-t-2 border-blue-600 rounded-md text-blue-900 font-extrabold">
                 Client-centric approach
               </h1>
               <div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
@@ -71,7 +83,7 @@ const Grid = () => {
             </Link>
             {/* second */}
             <Link href="/" aria-label="View Item">
-              <h1 className="text-center py-2 text-CardHeading border-t-2 border-blue-600 rounded-md text-blue-700 font-extrabold">
+              <h1 className="text-center py-2 text-CardHeading border-t-2 border-blue-600 rounded-md text-blue-900 font-extrabold">
                 Iterative Development
               </h1>
               <div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl text-center ">
@@ -99,7 +111,7 @@ const Grid = () => {
             </Link>
             {/* third */}
             <Link href="/" aria-label="View Item">
-              <h1 className="text-center py-2 text-CardHeading border-t-2 border-blue-600 rounded-md text-blue-700 font-extrabold">
+              <h1 className="text-center py-2 text-CardHeading border-t-2 border-blue-600 rounded-md text-blue-900 font-extrabold">
                 Incremental Delivery
               </h1>
               <div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl text-center">
@@ -130,7 +142,7 @@ const Grid = () => {
           <div className="grid gap-6 row-gap-5  lg:grid-cols-3 sm:row-gap-6 sm:grid-cols-2 ">
             {/* first */}
             <Link href="/" aria-label="View Item">
-              <h1 className="text-center py-2 text-CardHeading border-t-2 border-blue-600 rounded-md text-blue-700 font-extrabold">
+              <h1 className="text-center py-2 text-CardHeading border-t-2 border-blue-600 rounded-md text-blue-900 font-extrabold">
                 Innovative Solutions
               </h1>
               <div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl text-center">
@@ -157,7 +169,7 @@ const Grid = () => {
             </Link>
             {/* second */}
             <Link href="/" aria-label="View Item">
-              <h1 className="text-center py-2 text-CardHeading border-t-2 border-blue-600 rounded-md text-blue-700 font-extrabold">
+              <h1 className="text-center py-2 text-CardHeading border-t-2 border-blue-600 rounded-md text-blue-900 font-extrabold">
                 Agile Methodology
               </h1>
               <div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl text-center">
@@ -185,7 +197,7 @@ const Grid = () => {
             </Link>
             {/* third */}
             <Link href="/" aria-label="View Item">
-              <h1 className="text-center py-2 text-CardHeading border-t-2 border-blue-600 rounded-md text-blue-700 font-extrabold">
+              <h1 className="text-center py-2 text-CardHeading border-t-2 border-blue-600 rounded-md text-blue-900 font-extrabold">
                 Cross-functional teams
               </h1>
               <div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
